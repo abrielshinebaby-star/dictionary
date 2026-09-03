af1233e7b808f91e2e06444dd36bf7588321c3d5
abrielshinebaby-star <abrielshinebaby@gmail.com>
2026-09-03 20:02:13 +0200

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,13 @@
+dictionary={"India":"New Delhi","Netherlands":"Amsterdam","U.S.A":"Washington D.C"}
+print(dictionary["U.S.A"])
+print(len(dictionary))
+dictionary["Japan"]="Tokyo"
+print(dictionary)
+del dictionary["U.S.A"]
+print(dictionary)
+for key in dictionary:
+    print(key)
+for key in dictionary.keys():
+    print(key)
+for value in dictionary.values():
+    print(value)
